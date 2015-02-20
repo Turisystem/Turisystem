dbc805b486489e6ff4a434af6b3469bde07b833c
Rui Santos <rsconsulting@outlook.com>
2015-02-20 11:45:31 +0000

    Versão original
    Primeira versão - base de trabalho

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,22 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  Trajectos */
+    
+    
+        /* button  Sobre */
+    $(document).on("click", ".uib_w_5", function(evt)
+    {
+        /* your code goes here */ 
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
